07af2b7920cedf2758af6313e17ff67ebb7b7e71
Brian Wong <brianw7218@gmail.com>
2023-02-14 22:27:18 -0800

    Create a preliminary outline of components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <h1>Genshin Impact Memory Game</h1>
+      <p>Test your memory!</p>
+      <p>ScoresDisplay</p>
+      <p>ElementDropdownMenu</p>
+      <p>CardsContainer</p>
+      <p>All assets by HoYoverse</p>
+    </>
+  );
 }
 
 export default App;
