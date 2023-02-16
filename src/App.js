60accff6ed943b669753000129617ff1ca9e9eea
Brian Wong <brianw7218@gmail.com>
2023-02-15 21:06:57 -0800

    Move score state to App from score display component, and divide best scores by element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScores, setBestScores] = useState({
+    pyro: 0,
+    hydro: 0,
+    anemo: 0,
+    electro: 0,
+    dendro: 0,
+    cryo: 0,
+    geo: 0,
+  });
   return (
     <>
       <h1>Genshin Impact Memory Game</h1>
