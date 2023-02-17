cc8ad27a118857b361d8afbe590a54494fd6d12b
Brian Wong <brianw7218@gmail.com>
2023-02-16 21:05:13 -0800

    Add more character data for testing

--- src/characterData.js
+++ src/characterData.js
@@ -1,6 +1,16 @@
 export const characterData = {
   pyro: [
     {
+      name: "Amber",
+      image:
+        "https://static.wikia.nocookie.net/gensin-impact/images/a/a6/Icon_Emoji_012_Amber_Outta_my_way%21.png/",
+    },
+    {
+      name: "Bennett",
+      image:
+        "https://static.wikia.nocookie.net/gensin-impact/images/6/6f/Icon_Emoji_Paimon%27s_Paintings_20_Bennett_1.png/",
+    },
+    {
       name: "Diluc",
       image:
         "https://static.wikia.nocookie.net/gensin-impact/images/1/10/Icon_Emoji_Paimon%27s_Paintings_Diluc_1.png/",
