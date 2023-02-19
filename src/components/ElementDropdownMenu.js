910f0c599189c3160b89ed7082d51137fcf639af
Brian Wong <brianw7218@gmail.com>
2023-02-18 22:31:24 -0800

    Add class names to dropdown menu elements

--- src/components/ElementDropdownMenu.js
+++ src/components/ElementDropdownMenu.js
@@ -5,28 +5,63 @@ const ElementDropdownMenu = memo(function ElementDropdownMenu({
 }) {
   return (
     <div className="element-menu">
-      <button>Element</button>
+      <button className="placeholder-button elemental-button">Element</button>
       <ul className="element-dropdown">
         <li className="menu-option">
-          <button onClick={() => changeElement("pyro")}>Pyro</button>
+          <button
+            className="pyro-background elemental-button"
+            onClick={() => changeElement("pyro")}
+          >
+            Pyro
+          </button>
         </li>
         <li className="menu-option">
-          <button onClick={() => changeElement("hydro")}>Hydro</button>
+          <button
+            className="hydro-background elemental-button"
+            onClick={() => changeElement("hydro")}
+          >
+            Hydro
+          </button>
         </li>
         <li className="menu-option">
-          <button onClick={() => changeElement("anemo")}>Anemo</button>
+          <button
+            className="anemo-background elemental-button"
+            onClick={() => changeElement("anemo")}
+          >
+            Anemo
+          </button>
         </li>
         <li className="menu-option">
-          <button onClick={() => changeElement("electro")}>Electro</button>
+          <button
+            className="electro-background elemental-button"
+            onClick={() => changeElement("electro")}
+          >
+            Electro
+          </button>
         </li>
         <li className="menu-option">
-          <button onClick={() => changeElement("dendro")}>Dendro</button>
+          <button
+            className="dendro-background elemental-button"
+            onClick={() => changeElement("dendro")}
+          >
+            Dendro
+          </button>
         </li>
         <li className="menu-option">
-          <button onClick={() => changeElement("cryo")}>Cryo</button>
+          <button
+            className="cryo-background elemental-button"
+            onClick={() => changeElement("cryo")}
+          >
+            Cryo
+          </button>
         </li>
         <li className="menu-option">
-          <button onClick={() => changeElement("geo")}>Geo</button>
+          <button
+            className="geo-background elemental-button"
+            onClick={() => changeElement("geo")}
+          >
+            Geo
+          </button>
         </li>
       </ul>
     </div>
