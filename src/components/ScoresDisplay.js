4d48e0dd745607d02ae33ba9996b524daac51abb
Brian Wong <brianw7218@gmail.com>
2023-02-18 16:40:35 -0800

    Add div tags and class name

--- src/components/ScoresDisplay.js
+++ src/components/ScoresDisplay.js
@@ -1,11 +1,11 @@
 const ScoresDisplay = ({ currentScore, bestScore, element }) => {
   return (
-    <>
+    <div className="scores-display">
       <p>{`Best ${
         element.charAt(0).toUpperCase() + element.slice(1)
       } Score: ${bestScore}`}</p>
       <p>{`Current Score: ${currentScore}`}</p>
-    </>
+    </div>
   );
 };
 
