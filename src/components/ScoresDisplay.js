e6f31d7c4d42d63c2801ec4176d23bce0b783987
Brian Wong <brianw7218@gmail.com>
2023-02-18 21:28:50 -0800

    Start to add color styling based on Genshin colors

--- src/components/ScoresDisplay.js
+++ src/components/ScoresDisplay.js
@@ -1,9 +1,12 @@
 const ScoresDisplay = ({ currentScore, bestScore, element }) => {
+  const elementLabel = element.charAt(0).toUpperCase() + element.slice(1);
   return (
     <div className="scores-display">
-      <p>{`Best ${
-        element.charAt(0).toUpperCase() + element.slice(1)
-      } Score: ${bestScore}`}</p>
+      <p>
+        Best{" "}
+        <span className={`${element}-text elemental-text`}>{elementLabel}</span>{" "}
+        Score: {bestScore}
+      </p>
       <p>{`Current Score: ${currentScore}`}</p>
     </div>
   );
