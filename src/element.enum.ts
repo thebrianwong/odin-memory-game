a9fbb2647ae509c4da5dbc87b7d38e2ba9be3a80
Brian Wong <brianw7218@gmail.com>
2023-06-08 21:18:26 -0700

    Add an enum for Genshin element names

---
+++ src/element.enum.ts
@@ -0,0 +1,11 @@
+enum Element {
+  PYRO = "pyro",
+  HYDRO = "hydro",
+  ANEMO = "anemo",
+  ELECTRO = "electro",
+  DENDRO = "dendro",
+  CRYO = "cryo",
+  GEO = "geo",
+}
+
+export default Element;
