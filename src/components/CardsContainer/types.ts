f482bf7c13fef10838985d6ce6b14f6faf371885
Brian Wong <brianw7218@gmail.com>
2023-06-08 21:22:33 -0700

    Fix typing issue for CardsContainer component props

--- src/components/CardsContainer/types.ts
+++ src/components/CardsContainer/types.ts
@@ -1,7 +1,9 @@
+import Element from "../../element.enum";
+
 type CardsContainerProps = {
-  element: string;
+  element: Element;
   characterData: Array<{ name: string }>;
-  resolveCardChoice: (characterName: string, element: string) => void;
+  resolveCardChoice: (characterName: string, element: Element) => void;
 };
 
 export default CardsContainerProps;
